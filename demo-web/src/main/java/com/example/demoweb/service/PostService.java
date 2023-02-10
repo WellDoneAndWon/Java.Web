3b481e72fb2280f46a4f16941a57fa0256737550
WellDoneAndWon <ildanaxy@gmail.com>
2023-02-10 22:42:04 +0500

    Changed html file and updated last code

--- demo-web/src/main/java/com/example/demoweb/service/PostService.java
+++ demo-web/src/main/java/com/example/demoweb/service/PostService.java
@@ -4,18 +4,13 @@ import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
-    public Iterable<Post> listAllPosts(){
-        List<Post> posts = new ArrayList<>();
-        Post post1 = new Post("What's up?");
-        Post post2 = new Post("How are you?");
-        Post post3 = new Post("How you doing?");
-        posts.add(post1);
-        posts.add(post2);
-        posts.add(post3);
+    List<Post> posts = new ArrayList<>();
+    public List<Post> listAllPosts(){
         return posts;
     }
 }
