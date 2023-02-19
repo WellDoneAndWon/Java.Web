7250ff1ec99a885ac2afdf517783fbdea17c0023
WellDoneAndWon <ildanaxy@gmail.com>
2023-02-20 00:19:32 +0500

    9.2 added button

--- demo-web/src/main/java/com/example/demoweb/repository/PostRepository.java
+++ demo-web/src/main/java/com/example/demoweb/repository/PostRepository.java
@@ -2,11 +2,7 @@ package com.example.demoweb.repository;
 
 import com.example.demoweb.model.Post;
 import org.springframework.data.repository.CrudRepository;
-<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
-
-=======
->>>>>>> origin/master
 
 public interface PostRepository extends CrudRepository<Post, Long> {
 }
